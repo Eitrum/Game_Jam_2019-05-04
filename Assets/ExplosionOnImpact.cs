2f4a78f4e77b133bd303bd6c7ae8f4546e154a90
Björn Englesson <benglesson@gmail.com>
2019-05-04 18:19:53 +0200

    Add explosion to ISS

---
+++ Assets/ExplosionOnImpact.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionOnImpact : MonoBehaviour
+{
+    public ParticleSystem explosionParticle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        explosionParticle.Play();
+    }
+}

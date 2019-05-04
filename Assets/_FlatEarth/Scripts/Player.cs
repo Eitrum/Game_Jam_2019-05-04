6a00b2716008f6568d192b726e35341d3843c0cc
Björn Englesson <benglesson@gmail.com>
2019-05-04 14:12:42 +0200

    Player stuff

--- Assets/_FlatEarth/Scripts/Player.cs
+++ Assets/_FlatEarth/Scripts/Player.cs
@@ -8,11 +8,15 @@ public sealed class Player : MonoBehaviour
     private Rewired.Player player;
     private Vector3 moveVector;
 
+    public bool canMove;
+
+#pragma warning disable
     [Header("Components")]
     [SerializeField] private Rigidbody rb;
 
     [Header("Settings")]
     [SerializeField] private PlayerMovementSettings movementSettings;
+#pragma warning enable
 
     void Awake()
     {
@@ -24,6 +28,22 @@ public sealed class Player : MonoBehaviour
         moveVector.x = player.GetAxis("Move Horizontal");
         moveVector.z = player.GetAxis("Move Vertical");
 
-        rb.AddForce(moveVector.normalized * movementSettings.moveSpeed * Time.deltaTime, ForceMode.Force);
+        if (canMove)
+        {
+            rb.AddForce(moveVector.normalized * movementSettings.moveSpeed * Time.deltaTime, ForceMode.Force);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody)
+        {
+
+            Vector3 velocity = collision.rigidbody.velocity;
+            Vector3 direction = (rb.position - collision.rigidbody.position).normalized;
+            Vector3 force = direction * Mathf.Clamp(velocity.magnitude, 2.5f, 5f);
+            rb.AddForce(force, 
+                ForceMode.Impulse);
+        }
     }
 }

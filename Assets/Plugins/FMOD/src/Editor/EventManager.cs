ebc47bdff354fdcf31791947d5827aec82d7afda
Lunaea_Eitrum <sebbbe97@hotmail.com>
2019-05-05 14:40:49 +0200

    FMod fix

--- Assets/Plugins/FMOD/src/Editor/EventManager.cs
+++ Assets/Plugins/FMOD/src/Editor/EventManager.cs
@@ -523,8 +523,8 @@ namespace FMODUnity
                 foreach (var bankRef in eventCache.EditorBanks)
                 {
                     var subDir = Path.GetDirectoryName(bankRef.Path).Replace(bankSourceFolder, "");
-                    string sourcePath = bankSourceFolder + subDir + Path.DirectorySeparatorChar + bankRef.Name + ".bank";
-                    string targetPath = bankTargetFolder + subDir + Path.DirectorySeparatorChar + bankRef.Name + "." + bankTargetExension;
+                    string sourcePath = bankSourceFolder + Path.DirectorySeparatorChar + bankRef.Name + ".bank";
+                    string targetPath = bankTargetFolder + Path.DirectorySeparatorChar + bankRef.Name + "." + bankTargetExension;
 
                     FileInfo sourceInfo = new FileInfo(sourcePath);
                     FileInfo targetInfo = new FileInfo(targetPath);
@@ -541,6 +541,7 @@ namespace FMODUnity
                         {
                             targetInfo.IsReadOnly = false;
                         }
+                        Debug.Log(sourcePath);
                         File.Copy(sourcePath, targetPath, true);
                         targetInfo = new FileInfo(targetPath);
                         targetInfo.IsReadOnly = false;
